1b599f375b3242f97e8e383b66e40238f78baa3d
retr0lbb <retr0lbb@gmail.com>
2026-01-20 20:25:09 -0300

    feat: fix infinit scroll

--- src/features/register/select-played-games.tsx
+++ src/features/register/select-played-games.tsx
@@ -64,33 +64,34 @@ export function SelectPlayedGames() {
       </div>
 
       <div className="flex-1 columns-2 space-y-3 overflow-y-auto relative">
-        {!isLoading ? (
-          games.map((game) => (
-            <div
-              key={game.id}
-              className="flex flex-col gap-2 overflow-hidden rounded-xl border border-slate-800 bg-slate-800/20 transition hover:border-blue-600/60"
-            >
-              <Image
-                src={game.imageUrl}
-                width={360}
-                height={480}
-                alt={game.name}
-                className="h-48 w-full object-cover"
-              />
+        {games.map((game) => (
+          <div
+            key={game.id}
+            className="flex flex-col gap-2 overflow-hidden rounded-xl border border-slate-800 bg-slate-800/20 transition hover:border-blue-600/60"
+          >
+            <Image
+              src={game.imageUrl}
+              width={360}
+              height={480}
+              alt={game.name}
+              className="h-48 w-full object-cover"
+            />
 
-              <div className="flex justify-center pt-2">
-                <p className="text-center text-lg font-medium text-slate-200">
-                  {game.name}
-                </p>
-              </div>
-
-              <div className="flex items-center justify-center border-t border-slate-800 bg-blue-600/10 pb-2 text-2xl text-slate-200 hover:opacity-70 active:opacity-60">
-                +
-              </div>
+            <div className="flex justify-center pt-2">
+              <p className="text-center text-lg font-medium text-slate-200">
+                {game.name}
+              </p>
             </div>
-          ))
-        ) : (
-          <Loading />
+
+            <div className="flex items-center justify-center border-t border-slate-800 bg-blue-600/10 pb-2 text-2xl text-slate-200 hover:opacity-70 active:opacity-60">
+              +
+            </div>
+          </div>
+        ))}
+        {isLoading && (
+          <div className="col-span-full flex justify-center py-4">
+            <Loading />
+          </div>
         )}
         <div ref={loadMoreRef} className="h-10 w-full" />
       </div>
